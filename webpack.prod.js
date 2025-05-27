3a449346a0f32f20343ff10cbc3458a80e4ac482
kharisma040504 <kharismamlda04@gmail.com>
2025-05-27 16:41:54 +0700

    intermediate

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,13 @@ const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  performance: {
+    hints: false,
+  },
   module: {
     rules: [
       {
@@ -29,11 +32,44 @@ module.exports = merge(common, {
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
-    new InjectManifest({
-      swSrc: "./src/public/service-worker.js",
-      swDest: "service-worker.js",
-      exclude: [/\.map$/, /manifest$/, /\.htaccess$/],
-      maximumFileSizeToCacheInBytes: 5000000,
+    new GenerateSW({
+      swDest: "workbox-sw.js",
+      clientsClaim: true,
+      skipWaiting: true,
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/story-api\.dicoding\.dev\/v1\//,
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api-cache",
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 24,
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "image-cache",
+            expiration: {
+              maxEntries: 60,
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+            },
+          },
+        },
+        {
+          urlPattern: /\.(?:js|css)$/,
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "static-resources",
+          },
+        },
+      ],
     }),
   ],
 });
